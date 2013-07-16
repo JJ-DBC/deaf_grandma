6350f6c8fb41d6414f56d9788042ef3c71107156
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-15 21:15:08 -0500

    Finish deaf grandma

--- deaf_grandma_skeleton/app/controllers/index.rb
+++ deaf_grandma_skeleton/app/controllers/index.rb
@@ -5,5 +5,10 @@ get '/' do
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  if params[:user_input] == params[:user_input].downcase
+    @grandma = "SPEAK UP YOU LITTLE RASCAL!"
+  else
+    @grandma = "NO NOT SINCE THE WAR!!!"
+  end
+  erb :index
 end
